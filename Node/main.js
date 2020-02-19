4c3f2b8dc770c8bed086ad04890c5b7103f11477
CyberCABAL <agent.n26@gmail.com>
2020-02-19 14:29:44 +0100

    NPM and further server examples.

--- Node/main.js
+++ Node/main.js
@@ -1,6 +1,7 @@
 
 const express = require("express");	// Express is for easy server setup.
 const WebSocket = require("ws");	// Websocket library.
+const request = require("request");	// HTTP library.
 const app = express();
 const server_port = 8080;
 
@@ -23,5 +24,13 @@ app.post("/data", (req, res) => {
 	console.log("Received POST:", req.body.data);
 	res.status(200).send()
 });
+
+// This is from https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
+// Use this 
+	/*request('http://url', { json: true }, (err, res, body) => {
+	  if (err) { return console.log(err); }
+	  console.log(body.url);
+	  console.log(body.explanation);
+	});*/
 
 app.listen(server_port, () => console.log("Running on port " + server_port))
